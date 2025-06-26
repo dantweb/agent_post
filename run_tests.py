ae08f2f1821bf6f3665dec202416e0867f42acb5
dantweb <daniil.tkachev@gmail.com>
2025-06-26 18:08:40 +0200

    automatic development - fix tests

--- run_tests.py
+++ run_tests.py
@@ -2,12 +2,13 @@ import sys
 import os
 import unittest
 
-src_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src'))
+src_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'src'))
 sys.path.append(src_dir)
 
 if __name__ == '__main__':
     loader = unittest.TestLoader()
     tests = loader.discover('tests')
-    print(f"Discovered tests: {tests}")  # Added print statement here
-    testRunner = unittest.runner.TextTestRunner()
-    testRunner.run(tests)
+    print(f"Discovered tests: {tests}")
+    runner = unittest.TextTestRunner()
+    result = runner.run(tests)
+    sys.exit(not result.wasSuccessful())
